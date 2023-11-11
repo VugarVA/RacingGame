98e931ea79b744d61aedc00072c8796f8d6868a1
Vugar Amirov <psyliney2016@gmail.com>
2023-11-11 12:08:00 -0500

    Create Checkpoints.cs

---
+++ Checkpoints.cs
@@ -0,0 +1,9 @@
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class Checkpoints : MonoBehaviour 
+{ 
+    public int cpNumber; // variable to keep track of the checkpoint number for each of the checkpoints 
+    // it is in a seperate class so that it can be attached to ecah of the checkpoints and assigned a different value 
+} 
